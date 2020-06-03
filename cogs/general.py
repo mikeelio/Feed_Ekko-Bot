7e13342fd3902b2e5f4423af6ec172b7e69dfc44
mikeelio <31973615+mikeelio@users.noreply.github.com>
2020-06-02 21:38:26 -0700

    Updated General

--- cogs/general.py
+++ cogs/general.py
@@ -2,7 +2,7 @@ import discord
 import os
 from discord.ext import commands
 
-class General (commands.Cog):
+class General(commands.Cog):
 
     def __init__(self, client):
         self.client = client
@@ -17,20 +17,23 @@ class General (commands.Cog):
             await ctx.send(f'{member.mention} Pong! ```That Ping took: {round(self.client.latency * 1000)}ms```')
 
     #Discord link
-    @commands.command()
-    async def joindiscord(self, ctx):
-        member = ctx.author
-        await member.send("Personal Streaming Server\n http://gestyy.com/w7ViEY")
-        await ctx.send(f"📬 You got mail")
+    # @commands.command()
+    # async def joindiscord(self, ctx):
+    #     member = ctx.author
+    #     await member.send("Personal Streaming Server\n http://gestyy.com/w7ViEY")
+    #     await ctx.send(f"📬 You got mail")
+
+    # @commands.command()
+    # async def invite(self, ctx):
+    #     member = ctx.author
+    #     await member.send("http://gestyy.com/w7ViTe")
+    #     await ctx.send(f"📬 You got mail")
 
     @commands.command()
-    async def invite(self, ctx):
-        member = ctx.author
-        await member.send("http://gestyy.com/w7ViTe")
-        await ctx.send(f"📬 You got mail")
-
-    @commands.command()
+    @commands.has_permissions(manage_messages=True)
     async def clear (self, ctx, amount=10):
         await ctx.channel.purge(limit=amount)
+
+
 def setup(client):
     client.add_cog(General(client))
